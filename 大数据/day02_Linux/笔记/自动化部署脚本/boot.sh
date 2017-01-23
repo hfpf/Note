4bf0f7b94e915150eb427b076f487159b62788fe
李欣 <li1191863273@hotmail.com>
2017-01-23 14:47:45 +0800

    u

--- 大数据/day02_Linux/笔记/自动化部署脚本/boot.sh
+++ 大数据/day02_Linux/笔记/自动化部署脚本/boot.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 #本脚本功能为配置ssh，执行安装脚本
+#需要做初始化工作：
+#ssh-keygen				#生成ssh密匙
+#yum install expect		#安装except命令
 
-SERVERS="node-3.itcast.cn node-4.itcast.cn"		#每个主机名
-PASSWORD=123456									#ssh免密登录的保护密码
-INSTALL_FILE=install_everyone.sh				#安装脚本
+SERVERS="centos05"								#每个主机名
+PASSWORD=123456									#每个主机密码
+INSTALL_FILE=install_jdk.sh				#安装脚本
 
 #单个设置ssh免密登录过程
 auto_ssh_copy_id() {
@@ -25,10 +27,12 @@ ssh_copy_id_to_all() {
     done
 }
 
-ssh_copy_id_to_all
+ssh_copy_id_to_all	#向每个主机发送公钥
 
 for SERVER in $SERVERS
 do
-    scp $INSTALL_FILE root@$SERVER:/root	#复制安装脚本
-    ssh root@$SERVER /root/$INSTALL_FILE	#执行安装脚本
+	#安装JDK
+    scp $INSTALL_FILE root@$SERVER:/root			#复制安装脚本
+	ssh root@$SERVER chmod +x /root/$INSTALL_FILE	
+    ssh root@$SERVER /root/$INSTALL_FILE			#执行安装脚本
 done
--- 大数据/day02_Linux/笔记/自动化部署脚本/install_jdk.sh
+++ 大数据/day02_Linux/笔记/自动化部署脚本/install_jdk.sh
@@ -8,7 +8,11 @@ yum install -y wget
 wget $JDK_LINK
 tar -zxvf jdk-8u112-linux-x64.tar.gz -C /usr/local
 cat >> /etc/profile << EOF
+
 export JAVA_HOME=/usr/local/jdk1.8.0_112
 export PATH=\$PATH:\$JAVA_HOME/bin
 EOF
+source /etc/profile
 
+echo "JDK安装完成"
+java -version
